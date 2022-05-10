04a891191fdc7a9dab0ab55d538464d33a35f1f4
pratik-k <prakar07@protonmail.com>
2022-05-10 18:48:44 +0530

    fix placemet of ships and recieve attack method

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,19 +1,33 @@
 export function Board(size=5){
     const board=Array(size).fill(0).map(()=>Array(size).fill(0))
-    const place=(ship,[x,y])=>{
-        if(ship.isHorizontal){
-            if(!(ship.length+x<size)){throw new TypeError("ship cant be place outside the board")};
-            for(let i=0;i<=ship.length;i++){
-                board[x][y]=ship
+    const ships=[]
+    const place=(ship,horizontal,x,y)=>{
+        if(ship.length+x>=size || ship.length+y>=size){
+            throw new Error("ship cant be place outside the board")
+        };
+        if(horizontal){
+            for(let i=0;i<ship.length;i++){
+                board[y][x]=ship[i]
                 x++
             }
         }else{
-            if(!(ship.length+y<size)){ throw new Error("ship cant be place outside the board")};
-            for(let i=0;i<=ship.length;i++){
-                board[x][y]=ship
+            for(let i=0;i<ship.length;i++){
+                board[y][x]=ship[i]
                 y++
             }
         }
+        ships.push(ship)
     }
-    return {place}
+    const recieveAttack=(x,y)=>{
+        // console.log(board[y][x])
+        if(board[y][x]==0){
+            board[y][x]=1
+        }else{
+            const hitShipPart=board[y][x]
+            hitShipPart.hit()
+        }
+    }
+    const isAllSunk=()=>ships.every((ship)=>ship.isSunk())
+    const getTile=(x,y)=>board[y][x]
+    return {place,getTile,recieveAttack,size:board.length}
 }
