b23e0df2b0ebe19d88e7cabc8ce80c58b6239470
pratik-k <prakar07@protonmail.com>
2022-05-10 18:52:42 +0530

    add initial ai player class extending from player

---
+++ src/aiPlayer.js
@@ -0,0 +1,14 @@
+import { Player } from "./player";
+
+class AiPlayer extends Player{
+    makeMove(){
+        let chosenTileCoords
+        do{
+        chosenTileCoords=[Math.floor(Math.random()*this.enemy.board.size),
+            Math.floor(Math.random()*this.enemy.board.size)]
+        }while(this.enemy.board.getTile(...chosenTileCoords)==1 ||
+               this.enemy.board.getTile(...chosenTileCoords).isHit==true)
+        
+        this.enemy.recieveAttack(...chosenTileCoords)
+    }
+}
