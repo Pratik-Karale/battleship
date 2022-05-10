cadf88e59ea694c5c06fbe897ea13b86c02b5f00
pratik-k <prakar07@protonmail.com>
2022-05-10 18:50:35 +0530

    make ship's parts thier own objects and allow them to have a hit method

--- src/ship.js
+++ src/ship.js
@@ -1,11 +1,30 @@
-const Ship=(length,startPos,horizontal=false)=>{
-    const damagedParts=Array(length)
-    const hit=(partNum)=>{
-        damagedParts[partNum]=1
+const ShipPart=()=>{
+    return{
+        isHit:false,
+        hit(){
+            this.isHit=true
+        }
+    }
+}
+
+const Ship=(length,horizontal=false)=>{
+    const daShip=Array(length).fill(0).map(()=>{
+        return ShipPart()
+    })
+    // console.log(daShip)
+    const hit=(pos)=>{
+        if(isFinite(pos)){
+            daShip[pos].hit()
+        }else{
+            const [x,y]=pos
+            daShip[y][x].hit()
+        }
     }
     const isSunk=()=>{
-        return damagedParts.every((damagedPart)=>damagedPart==1)
+        return daShip.every((part)=>part.isHit)
     }
-    return {length,hit,isSunk,isHorizontal:horizontal,startPos}
+    daShip.hit=hit
+    daShip.isSunk=isSunk
+    return     daShip
 };
 export {Ship}
