e6306c5735e2eabdc83d081865f931ef1818633a
pratik-k <prakar07@protonmail.com>
2022-05-10 23:13:24 +0530

    add state to the board obj

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,6 +1,7 @@
 export function Board(size=5){
     const board=Array(size).fill(0).map(()=>Array(size).fill(0))
     const ships=[]
+    const state={hits:[],misses:[],shipParts:[]}
     const place=(ship,horizontal,x,y)=>{
         if(ship.length+x>=size || ship.length+y>=size){
             throw new Error("ship cant be place outside the board")
@@ -8,11 +9,13 @@ export function Board(size=5){
         if(horizontal){
             for(let i=0;i<ship.length;i++){
                 board[y][x]=ship[i]
+                state.shipParts.push([x,y])
                 x++
             }
         }else{
             for(let i=0;i<ship.length;i++){
                 board[y][x]=ship[i]
+                state.shipParts.push([x,y])
                 y++
             }
         }
@@ -21,13 +24,15 @@ export function Board(size=5){
     const recieveAttack=(x,y)=>{
         // console.log(board[y][x])
         if(board[y][x]==0){
+            state.misses.push([x,y])
             board[y][x]=1
-        }else{
+        }else if(board[y][x]!=1){
+            state.hits.push([x,y])
             const hitShipPart=board[y][x]
             hitShipPart.hit()
         }
     }
     const isAllSunk=()=>ships.every((ship)=>ship.isSunk())
     const getTile=(x,y)=>board[y][x]
-    return {place,getTile,recieveAttack,size:board.length,isAllSunk}
+    return {place,getTile,recieveAttack,size:board.length,isAllSunk,state}
 }
