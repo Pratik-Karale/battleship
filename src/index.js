db16878c2fc2333a82eeae325e874072f4a8833a
pratik-k <prakar07@protonmail.com>
2022-05-10 23:11:47 +0530

    show players board

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import {Board} from "./gameBoard"
 import {Ship} from "./ship"
+import { BoardElem } from "./domHandler"
+import { Player } from "./player"
+import { AiPlayer } from "./aiPlayer"
 
+const player=new Player(Board(10))
+// place ships in playeers board
+player.board.place(Ship(10),true,0,0)
 
-
-export {Board,Ship}
+player.boardElem=BoardElem(player.board.state)
+document.querySelector("friendly-container").appendChild(player.boardElem)
