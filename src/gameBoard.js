9d7cc5221a972a513f7543a9a9760a07f7b4f3c9
pratik <prakar57@gmail.com>
2022-06-19 17:01:50 +0530

    add getShipCoords, add canPlaceShip and refactor accordingly

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -2,37 +2,36 @@ export function Board(size=5){
     const board=Array(size).fill(0).map(()=>Array(size).fill(0))
     const ships=[]
     const state={hits:[],misses:[],shipParts:[]}
-    const place=(ship,direction,x,y)=>{
-        if((ship.length+x>size && direction=="horizontal") || (ship.length+y>size && direction=="vertical")){
-            throw new Error("ship cant be place outside the board")
-        };
-        if(direction=="horizontal"){
-            for(let i=0;i<ship.length;i++){
-                board[y][x]=ship[i]
-                state.shipParts.push([x,y])
-                x++
-            }
-        }else if(direction=="vertical"){
-            for(let i=0;i<ship.length;i++){
-                board[y][x]=ship[i]
-                state.shipParts.push([x,y])
-                y++
-            }
-        }else if(direction=="diagonalLTR"){
-            for(let i=0;i<ship.length;i++){
-                board[y][x]=ship[i]
-                state.shipParts.push([x,y])
-                y++
-                x++
-            }
-        }else if(direction=="diagonalRTL"){
-            for(let i=0;i<ship.length;i++){
-                board[y][x]=ship[i]
-                state.shipParts.push([x,y])
-                y++
-                x--
+
+    const getShipCoords=(shipLength,direction,x,y)=>{
+        const shipCoords=[]
+        for(let i=0;i<shipLength;i++){
+            if(direction=="horizontal"){
+                shipCoords.push([x+i,y])
+            }else if(direction=="vertical"){
+                shipCoords.push([x,y+i])
+            }else if(direction=="diagonalLTR"){
+                shipCoords.push([x+i,y+i])
+            }else if(direction=="diagonalRTL"){
+                shipCoords.push([x-i,y+i])
             }
         }
+        return shipCoords
+    }
+    const isTileAvailable=(x,y)=> board[y] && board[y][x] == 0;
+    const canPlaceShip=(shipLength,direction,x,y)=>{
+        return getShipCoords(shipLength,direction,x,y)
+                    .every((shipCoord)=>isTileAvailable(...shipCoord))
+    }
+    const place=(ship,direction,x,y)=>{
+        if(!canPlaceShip(ship.length,direction,x,y)){
+            throw new Error("ship cant be place outside the board")
+        };
+        getShipCoords(ship.length,direction,x,y).forEach((shipCoord,i)=>{
+            const [x,y]=shipCoord
+            board[y][x]=ship[i]
+            state.shipParts.push(shipCoord)
+        })
         ships.push(ship)
     }
     const recieveAttack=(x,y)=>{
@@ -49,5 +48,5 @@ export function Board(size=5){
     const isAllSunk=()=>ships.every((ship)=>ship.isSunk())
     const getTile=(x,y)=>board[y][x]
     const isTileEmpty=(x,y)=>board[y][x]==0 || board[y][x].isHit==false
-    return {place,getTile,recieveAttack,size:board.length,isAllSunk,state,isTileEmpty}
+    return {place,getTile,recieveAttack,size:board.length,isAllSunk,state,isTileEmpty,canPlaceShip,getShipCoords}
 }
